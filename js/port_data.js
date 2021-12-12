5b6aa2cc88baacf66e80f26b53af3cfc8315fe6b
smitkakadiya57 <76986207+smitkakadiya57@users.noreply.github.com>
2021-12-12 18:04:13 +0530

    Add files via upload

---
+++ js/port_data.js
@@ -0,0 +1,91 @@
+const port_data = [
+  {
+    id: 0,
+    p_img: "p2.png",
+    p_name: " Go Green ",
+    p_desc:
+      " This is website of an Farming company that farming the crops and also selling the agricultural equipment.This website connected with database this website able to place order online and also admin panel available for management ",
+    p_cre: "September 2021",
+    p_tech: "HTML, CSS, JS, PHP",
+    p_view_1: "https://github.com/smitkakadiya57/go-green-php",
+    p_view_2: "#",
+  },
+
+  {
+    id: 1,
+    p_img: "p3.png",
+    p_name: "Go Stream",
+    p_desc:
+      " This is music Streaming Website. This Provide basic music player with control like play, pause, etc.Also containe Playlist for songs. ",
+    p_cre: "September 2021",
+    p_tech: "HTML, CSS, JS",
+    p_view_1: "https://github.com/smitkakadiya57/GoStream",
+    p_view_2: "https://smitkakadiya57.github.io/GoStream/",
+  },
+  {
+    id: 2,
+    p_img: "p1.png",
+    p_name: " Gada Electronics",
+    p_desc:
+      "This is e-commerce website for electronics item shopping.it includes filter based on product and product full description also available. ",
+    p_cre: "June 2021",
+    p_tech: "HTML, CSS, JS",
+    p_view_1: "https://github.com/smitkakadiya57/gadaelectronics",
+    p_view_2: "https://smitkakadiya57.github.io/gadaelectronics/",
+  },
+  {
+    id: 3,
+    p_img: "p0.png",
+    p_name: "Invoice Generator",
+    p_desc:
+      " This is Billing Website that generate Consumer bill. and also give summary of that bill.This Website automatically fill the price of buying goods from it's tag no.this project is use for every shop. ",
+    p_cre: "July 2021",
+    p_tech: "HTML, CSS, JS",
+    p_view_1: "https://github.com/smitkakadiya57/INVOICE-GENERATOR",
+    p_view_2: "https://smitkakadiya57.github.io/INVOICE-GENERATOR/",
+  },
+  {
+    id: 4,
+    p_img: "p4.png",
+    p_name: " Quick Notes",
+    p_desc:
+      " This is notes taking website.That saves website notes to Storage.we can delete and insert content on notes also. ",
+    p_cre: "July 2021",
+    p_tech: "HTML, CSS, JS",
+    p_view_1: "https://github.com/smitkakadiya57/QuickNotes",
+    p_view_2: "https://smitkakadiya57.github.io/QuickNotes/",
+  },
+  {
+    id: 5,
+    p_img: "p5.png",
+    p_name: "Result Calculator",
+    p_desc:
+      " This is basic ressult calculator which calculate the result based on entered marks. we can also add more fields and remove that field also. ",
+    p_cre: "July 2021",
+    p_tech: "HTML, CSS, JS",
+    p_view_1: "https://github.com/smitkakadiya57/Result-Calculator",
+    p_view_2: "https://smitkakadiya57.github.io/ResultCalculator/",
+  },
+  {
+    id: 6,
+    p_img: "p6.png",
+    p_name: "Skill World",
+    p_desc:
+      " This is basic laboratory website. It contain form which can use to collect student admission data.",
+    p_cre: "May 2021",
+    p_tech: "HTML, CSS, JS, BOOTSTRAP",
+    p_view_1: "https://github.com/smitkakadiya57/SkillWorld",
+    p_view_2: " https://smitkakadiya57.github.io/SkillWorld/",
+  },
+  {
+    id: 7,
+    p_img: "p7.png",
+    p_name: " Skola e-Learning",
+    p_desc:
+      " This is Online Tutor Platform where student can study their respective subjects.It contains all information regarding teachers and educators. ",
+    p_cre: "June 2021",
+    p_tech: "HTML, CSS, JS, BOOTSTRAP",
+    p_view_1: "https://github.com/smitkakadiya57/SKOLA",
+    p_view_2: "https://smitkakadiya57.github.io/SKOLA/",
+  },
+];
